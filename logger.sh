7e2be0befa68691d08a14180905e55c2e890d94b
Aarish Gilani <aarishgilani10@gmail.com>
2023-03-02 18:53:01 -0600

    first commit

---
+++ logger.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Nice greeting for user
+echo "Hello Aarish, Good day today! Ready to work"
+
+# Get current day | From system
+current_day=$(date +'%A')
+
+
+# Return
+echo "Clocked you for $current_day"
